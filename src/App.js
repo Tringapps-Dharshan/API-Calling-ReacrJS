e6466af5d17b17d377ee06b79b12800b8022840b
Tringapps-Dharshan <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-04-29 11:53:23 +0530

    Uploaded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import APICall from './Component/APICall'
+import './Component/APICall.css'
+function App() {
+  return (
+    <div className="App">
+      <h1 className='heading'>Welcome to Tringapps !</h1>
+      <APICall/>
+    </div>
+  );
+}
+
+export default App;
