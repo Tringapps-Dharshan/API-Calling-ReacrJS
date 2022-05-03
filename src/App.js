5d3b6d0fb5852ff6cb872e4d6ba546ad5af61896
Dharshan A <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-05-03 20:53:23 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
+import React, { useEffect } from 'react';
 import './App.css';
-import APICall from './Component/APICall'
-import './Component/APICall.css'
-function App() {
-  return (
-    <div className="App">
-      <h1 className='heading'>Welcome to Tringapps !</h1>
-      <APICall/>
-    </div>
-  );
-}
+import { useState } from 'react';
 
+function App() {
+  const [selected, setSelected] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState([]);
+  const domain = 'https://reqres.in/';
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null)
+    }
+    setSelected(i)
+  }
+  useEffect(() => {
+    fetch(`${domain}api/users`)
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true);
+        setData(result.data);
+      }, (error) => {
+        setIsLoaded(true);
+        setError(error);
+      })
+
+  }, [])
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading....</div>
+  }
+  else {
+    return (
+      <div className="Wrapper">
+        <div className='accordian'>
+          {
+            data.map((element, i) => {
+              return <div className='item' key={element.id}>
+                <div className='title' onClick={() => toggle(i)}>
+                  <h2>{element.first_name}</h2>
+                  <span>{selected === i ? '-' : '+'}</span>
+                </div>
+                <div className={
+                  selected === i ? 'content show' : 'content'
+                }>
+                  <table>
+                    <tr>
+                      <td><img src={element.avatar} alt={element.first_name} /></td>
+                      <td className='details'>
+                        First name: {element.first_name}<br/>
+                        Last name: {element.last_name}<br/>
+                        Email: {element.email}<br/>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
+              </div>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
