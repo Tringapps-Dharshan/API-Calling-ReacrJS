1ce4eb18daf652e066a167c87401d2efa91939da
Tringapps-Dharshan <102800724+Tringapps-Dharshan@users.noreply.github.com>
2022-05-04 12:37:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,  useState } from 'react';
 import './App.css';
-import { useState } from 'react';
 
 function App() {
   const [selected, setSelected] = useState(null);
